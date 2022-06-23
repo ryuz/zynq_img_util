aa030baeac7c0fb9f02987f5f466ece4642f1901
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2022-06-24 08:11:19 +0900

    update

--- make_ultra96v2_debian_img.sh
+++ make_ultra96v2_debian_img.sh
@@ -16,6 +16,8 @@ if [ ! -f $TGZ_FILE ]; then
 fi
 tar zxvf $TGZ_FILE
 
+MNT_P1="/mnt/loop_img_p1"
+MNT_P2="/mnt/loop_img_p2"
 
 DEV_LOOP=`sudo losetup -f`
 
@@ -27,30 +29,66 @@ sudo parted $DEV_LOOP -s mklabel msdos -s mkpart primary fat32 1048576B 31562137
 sudo mkfs.vfat ${DEV_LOOP}p1
 sudo mkfs.ext4 ${DEV_LOOP}p2
 
-sudo mkdir -p /mnt/usb1
-sudo mkdir -p /mnt/usb2
-sudo mount ${DEV_LOOP}p1 /mnt/usb1
-sudo mount ${DEV_LOOP}p2 /mnt/usb2
+sudo mkdir -p $MNT_P1
+sudo mkdir -p $MNT_P2
+sudo mount ${DEV_LOOP}p1 $MNT_P1
+sudo mount ${DEV_LOOP}p2 $MNT_P2
 
 cd ZynqMP-FPGA-Linux-${VERSION}
-sudo cp target/Ultra96-V2/boot/* /mnt/usb1
+sudo cp target/Ultra96-V2/boot/* $MNT_P1
 
-sudo tar xfz debian11-rootfs-vanilla.tgz -C /mnt/usb2
-sudo mkdir /mnt/usb2/home/fpga/debian
-sudo cp linux-image-5.4.0-xlnx-v2020.2-zynqmp-fpga_5.4.0-xlnx-v2020.2-zynqmp-fpga-3_arm64.deb     /mnt/usb2/home/fpga/debian
-sudo cp linux-headers-5.4.0-xlnx-v2020.2-zynqmp-fpga_5.4.0-xlnx-v2020.2-zynqmp-fpga-3_arm64.deb   /mnt/usb2/home/fpga/debian
-sudo cp fclkcfg-5.4.0-xlnx-v2020.2-zynqmp-fpga_1.7.2-1_arm64.deb                                  /mnt/usb2/home/fpga/debian
-sudo cp u-dma-buf-5.4.0-xlnx-v2020.2-zynqmp-fpga_3.2.4-0_arm64.deb                                /mnt/usb2/home/fpga/debian
-sudo cp linux-image-5.10.0-xlnx-v2021.1-zynqmp-fpga_5.10.0-xlnx-v2021.1-zynqmp-fpga-4_arm64.deb   /mnt/usb2/home/fpga/debian
-sudo cp linux-headers-5.10.0-xlnx-v2021.1-zynqmp-fpga_5.10.0-xlnx-v2021.1-zynqmp-fpga-4_arm64.deb /mnt/usb2/home/fpga/debian
-sudo cp fclkcfg-5.10.0-xlnx-v2021.1-zynqmp-fpga_1.7.2-1_arm64.deb                                 /mnt/usb2/home/fpga/debian
-sudo cp u-dma-buf-5.10.0-xlnx-v2021.1-zynqmp-fpga_3.2.4-0_arm64.deb                               /mnt/usb2/home/fpga/debian
+sudo tar xfz debian11-rootfs-vanilla.tgz -C $MNT_P2
+sudo mkdir $MNT_P2/home/fpga/debian
+sudo cp linux-image-5.4.0-xlnx-v2020.2-zynqmp-fpga_5.4.0-xlnx-v2020.2-zynqmp-fpga-3_arm64.deb     $MNT_P2/home/fpga/debian
+sudo cp linux-headers-5.4.0-xlnx-v2020.2-zynqmp-fpga_5.4.0-xlnx-v2020.2-zynqmp-fpga-3_arm64.deb   $MNT_P2/home/fpga/debian
+sudo cp fclkcfg-5.4.0-xlnx-v2020.2-zynqmp-fpga_1.7.2-1_arm64.deb                                  $MNT_P2/home/fpga/debian
+sudo cp u-dma-buf-5.4.0-xlnx-v2020.2-zynqmp-fpga_3.2.4-0_arm64.deb                                $MNT_P2/home/fpga/debian
+sudo cp linux-image-5.10.0-xlnx-v2021.1-zynqmp-fpga_5.10.0-xlnx-v2021.1-zynqmp-fpga-4_arm64.deb   $MNT_P2/home/fpga/debian
+sudo cp linux-headers-5.10.0-xlnx-v2021.1-zynqmp-fpga_5.10.0-xlnx-v2021.1-zynqmp-fpga-4_arm64.deb $MNT_P2/home/fpga/debian
+sudo cp fclkcfg-5.10.0-xlnx-v2021.1-zynqmp-fpga_1.7.2-1_arm64.deb                                 $MNT_P2/home/fpga/debian
+sudo cp u-dma-buf-5.10.0-xlnx-v2021.1-zynqmp-fpga_3.2.4-0_arm64.deb                               $MNT_P2/home/fpga/debian
 
-sudo mkdir /mnt/usb2/mnt/boot
-sudo sh -c "cat <<EOT >> /mnt/usb2/etc/fstab
+sudo mkdir $MNT_P2/mnt/boot
+sudo sh -c "cat <<EOT >> $MNT_P2/etc/fstab
 /dev/mmcblk0p1  /mnt/boot   auto    defaults    0   0
 EOT"
 
+
+
+cd $WORD_DIR
+sudo cp ./boot/*       /mnt/usb1
+sudo cp resize2fs_once /mnt/usb2/etc/init.d/
+sudo chmod 755         /mnt/usb2/etc/init.d/resize2fs_once
+
+sudo cp setup.sh       /mnt/usb2/
+sudo chmod 755         /mnt/usb2/setup.sh
+sudo cp resize2fs_once /mnt/usb2/etc/init.d/
+sudo chmod 755         /mnt/usb2/etc/init.d/resize2fs_once
+
+sudo mv /mnt/usb2/etc/resolv.conf    /mnt/usb2/etc/resolv.conf.org
+sudo cp /etc/resolv.conf             /mnt/usb2/etc
+sudo cp /usr/bin/qemu-aarch64-static /mnt/usb2/usr/bin
+
+sudo chroot /mnt/usb2/ ./setup.sh
+
+sudo mv /mnt/usb2/etc/resolv.conf.org /mnt/usb2/etc/resolv.conf
+sudo rm /mnt/usb2/usr/bin/qemu-aarch64-static
+sudo rm /mnt/usb2/setup.sh
+
+df
+
+sync
 sudo umount /mnt/usb1
 sudo umount /mnt/usb2
 sudo losetup -d $DEV_LOOP
+
+
+
+sync
+sudo umount $MNT_P1
+sudo umount $MNT_P2
+sudo losetup -d $DEV_LOOP
+sync
+
+sudo tmdir -p $MNT_P1
+sudo tmdir -p $MNT_P2
---
+++ sd_to_img.sh
@@ -0,0 +1,87 @@
+
+#!/bin/bash
+
+# SDカードイメージをパーティーション拡張可能な形で再 img 化する
+
+########################################
+# 設定
+########################################
+
+DST_IMG="sd_card.img"
+if [ $# -eq 1 ]; then
+  DST_IMG=$1
+fi
+
+DATE_TAG=$(date +%Y%m%d%H%M)
+BOOT_TGZ="boot-$DATE_TAG.tgz"
+ROOTFS_TGZ="rootfs-$DATE_TAG.tgz"
+
+
+########################################
+# 既存SDを tgz にバックアップ
+########################################
+
+# バックアップ
+sudo tar zcf $BOOT_TGZ   -C /media/$USER/ULTRA96 .
+sudo tar zcf $ROOTFS_TGZ -C /media/$USER/rootfs  .
+
+
+
+########################################
+# 新しいイメージを作成
+########################################
+
+MNT_P1="/mnt/loop_img_p1"
+MNT_P2="/mnt/loop_img_p2"
+
+# 空いてるloopを得る
+DEV_LOOP=`sudo losetup -f`
+
+# 空のイメージを作る
+rm -f $DST_IMG
+truncate -s 4GiB $DST_IMG
+
+# パーティーションを作る
+sudo losetup $DEV_LOOP $DST_IMG
+sudo parted $DEV_LOOP -s mklabel msdos -s mkpart primary fat32 1048576B 315621375B -s mkpart primary ext4 315621376B 100% -s set 1 boot
+sudo mkfs.vfat ${DEV_LOOP}p1
+sudo mkfs.ext4 ${DEV_LOOP}p2
+
+# マウントする
+sudo mkdir -p $MNT_P1
+sudo mkdir -p $MNT_P2
+sudo mount ${DEV_LOOP}p1 $MNT_P1
+sudo mount ${DEV_LOOP}p2 $MNT_P2
+
+# ファイルコピー
+sudo tar zxf $BOOT_TGZ   -C $MNT_P1
+sudo tar zxf $ROOTFS_TGZ -C $MNT_P2
+
+# 自動パーティーション拡張を仕込む
+sudo cp resize2fs_once $MNT_P2/etc/init.d/
+sudo chmod 755         $MNT_P2/etc/init.d/resize2fs_once
+sudo cp setup.sh       $MNT_P2
+sudo chmod 755         $MNT_P2/setup.sh
+
+sudo mv /mnt/usb2/etc/resolv.conf    $MNT_P2/etc/resolv.conf.org
+sudo cp /etc/resolv.conf             $MNT_P2/etc
+sudo cp /usr/bin/qemu-aarch64-static $MNT_P2/usr/bin
+
+sudo chroot $MNT_P2 ./setup.sh
+
+sudo mv $MNT_P2/etc/resolv.conf.org /mnt/usb2/etc/resolv.conf
+sudo rm $MNT_P2/usr/bin/qemu-aarch64-static
+sudo rm $MNT_P2/setup.sh
+
+# サイズ表示
+df
+
+# アンマウント
+sync
+sudo umount $MNT_P1
+sudo umount $MNT_P2
+sudo losetup -d $DEV_LOOP
+sync
+
+sudo rmdir $MNT_P1
+sudo rmdir $MNT_P2
